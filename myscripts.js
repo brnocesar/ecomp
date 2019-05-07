9652258ddccbb2c103a1f1d60b414060101ab4b6
Bruno Cesar <bruunoocesar@gmail.com>
2019-05-07 10:48:56 -0300

    funcao recebe() é renomeada como recebe_teste(), definido que as tarefas seram adicionadas como linhas de uma tabela

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,12 @@
-function recebe() { /* recebe tarefa pelo campo de texto e manda para o campo 'a fazer' */  
+function recebe_teste() { /* recebe tarefa pelo campo de texto e manda para o campo 'a fazer' */  
     var tarefa = document.getElementById("entrada_tarefa").value;
     
     document.getElementById("a_fazer").innerHTML = tarefa;
+}
+
+function recebe(){ /* recebe tarefa pelo campo de texto, cria uma linha de tabela e coloca um campo de saida com a  tarefa  */
+
+
 }
 
 function muda(){
