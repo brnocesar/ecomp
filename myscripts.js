f16a313d00a4ba85cc474553ec841eb689c37778
Bruno Cesar <bruunoocesar@gmail.com>
2019-05-07 10:22:16 -0300

    correção da função recebe()

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,6 @@
-function recebe() { /* recebe tarefa pelo campo de texto e manda para a coluna 'a fazer' *//
+function recebe() { /* recebe tarefa pelo campo de texto e manda para o campo 'a fazer' */  
     var tarefa = document.getElementById("entrada_tarefa").value;
-
+    
     document.getElementById("a_fazer").innerHTML = tarefa;
 }
 
@@ -11,76 +11,3 @@ function muda(){
 function apaga(){
     
 }
-
-
-
-function recebe_texto() {
-    var texto = document.getElementById("entrada_texto").value;
-    var result;
-
-    // estilo do texto
-    var negrito = document.getElementById("negrito_check").checked;
-    var italico = document.getElementById("italico_check").checked;
-    var sublinhado = document.getElementById("sublinhado_check").checked;
-    if(negrito){
-        result = texto.bold();
-        texto = result;
-    }
-    else {
-        result = texto;
-    }
-    if(italico){
-        result = texto.italics();
-        texto = result;
-    }
-    else {
-        result = texto;
-    }
-    if(sublinhado){
-        result = "<u>"+texto+"</u>";
-        texto = result;
-    }
-    else {
-        result = texto;
-    }
-    
-    // cor do texto
-    var vermelho = document.getElementById("vermelho_check").checked;
-    var azul = document.getElementById("azul_check").checked;
-    var amarelo = document.getElementById("amarelo_check").checked;
-    if(vermelho){
-        result = texto.fontcolor("red")
-    }
-    else if(azul){
-        result = texto.fontcolor("blue")
-    }
-    else if(amarelo){
-        result = texto.fontcolor("yellow")
-    }
-    else {
-        result = texto;
-    }
-
-    // cor de fundo
-    var vermelho_fundo = document.getElementById("vermelho_fundo_check").checked;
-    var azul_fundo = document.getElementById("azul_fundo_check").checked;
-    var amarelo_fundo = document.getElementById("amarelo_fundo_check").checked;
-    if(vermelho_fundo){
-        document.getElementById("saida_texto").style.backgroundColor="red"
-    }
-    else if(azul_fundo){
-        document.getElementById("saida_texto").style.backgroundColor="blue"
-    }
-    else if(amarelo_fundo){
-        document.getElementById("saida_texto").style.backgroundColor="yellow"
-    }
-    else {
-        result = texto;
-    }
-    
-    document.getElementById("saida_texto").innerHTML = result;
-    
-}
-
-
-
