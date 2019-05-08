07f55151f5b2206dbcb8bfa6aa190f42392a9a1d
Bruno Cesar <bruunoocesar@gmail.com>
2019-05-08 15:56:58 -0300

    O contador não funcionava na forma como estava sendo colocado na atribuição (linha 13, primeiro commit de May 8), precisa converter explicitamente para String e concatenar. \n As funções recebe() e criar() estão funcionando como deveriam. A única ressalva é que criar() ainda está criando um elemento <output>, e como não consigo atribuir class='form-control' para eles vou fazer alterações nisso.

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,4 @@
-var contador = 0; /* variavel global para contar numero de tarefas */
+var contador = 0; /* variavel global para contar número de tarefas */
 
 function recebe_teste() { /* recebe tarefa pelo campo de texto e manda para o campo 'a fazer' */  
     var tarefa = document.getElementById("entrada_tarefa").value;
@@ -10,11 +10,11 @@ function criar(){ /* cria uma linha de tabela com um campo de output para texto*
     var campo_tarefa = document.createElement("output"); /* cria elemento output, onde será colocada a tarefa */
     campo_tarefa.setAttribute("type", "text");
     campo_tarefa.setAttribute("name", "saida");
-    campo_tarefa.setAttribute("id", "tarefa" + "contador++")
+    campo_tarefa.setAttribute("id", "tarefa" + String(contador++))
     //campo_tarefa.setAttribute("class", "form-control"); /* adiciona os atributos no elemento output*/ /* nao consigo atribuir a classe form-control a este elemento */
 
     var cell = document.createElement("td"); /* cria elemento td, célula da tabela */
-    cell.appendChild(campo_tarefa); /* coloca o nó de texto dentro do elemento td (celula) */
+    cell.appendChild(campo_tarefa); /* coloca o elemento output dentro do elemento td (celula) */
     
     var row = document.createElement("tr"); /* cria elemento tr, linha da tabela */
     row.appendChild(cell); /* coloca o elemento td dentro de tr */
@@ -26,9 +26,9 @@ function criar(){ /* cria uma linha de tabela com um campo de output para texto*
 }
 
 function recebe(){ /* recebe tarefa pelo campo de texto, chama funcao que cria uma linha na tabela com um campo de saida, e coloca a tarefa nesse campo */
-    var tarefa = document.getElementById("entrada_tarefa").value; /* recebe a tarefa pelo campo de input de texto e coloca na varialve tabela*/
-
-    document.getElementById(criar()).innerHTML = tarefa;
+    var tarefa = document.getElementById("entrada_tarefa").value; /* recebe a tarefa pelo campo de input de identificado por id="entrada_tarefa" */
+    
+    document.getElementById(criar()).innerHTML = tarefa; /* insere a tarefa no elemento criado por criar() */
 }
 
 function mudar(){
