592c5d3c8f1a7e2add39af939fdffbd528334793
Bruno Cesar <bruunoocesar@gmail.com>
2019-05-09 22:17:14 -0300

    Agora o elemento criado para conter o checkbox e a tarefa inserida é o elemento td. São atribuidas id's para os elementos td e input (checkbox) criados. Função criar() retorna o elemento td criado, e a função recebe() adiciona um elemento nó de texto (com a tarefa inserida) nesse elemento td.

--- myscripts.js
+++ myscripts.js
@@ -1,40 +1,34 @@
-var contador = 0; /* variavel global para contar número de tarefas */
+var global = 0; /* variavel global para contar número de tarefas criadas*/
 
 function recebe_teste() { /* recebe tarefa pelo campo de texto e manda para o campo 'a fazer' */  
     var tarefa = document.getElementById("entrada_tarefa").value;
-    
     document.getElementById("a_fazer").innerHTML = tarefa;
 }
 
-function criar(){ /* cria uma linha de tabela com um campo de output para texto*/
+function criar(){ /* cria uma linha/célula de tabela com um checkbox e retorna 'o elemento (?)' célula */
+    var contador = global++; /* incrementa o contador a cada nova tarefa */
+
     var seletor = document.createElement("input"); /* cria um elemento input */
     seletor.setAttribute("type", "checkbox"); /* define o elemento input como checkbox */
-    /* cria um elemento de/para texto, ou não, isso pode ser feito em recebe() */
-    /* faz as devidas atribuições */
-    var campo_tarefa = document.createElement("output"); /* cria elemento output, onde será colocada a tarefa */
-    campo_tarefa.setAttribute("type", "text");
-    campo_tarefa.setAttribute("name", "saida");
-    campo_tarefa.setAttribute("id", "tarefa" + String(contador++)); /* atribui o id em função do contador global */
+    seletor.setAttribute("id", "pfaz" + String(contador)); /* atribui o id em função do contador global */
     
-    /* o id devera ser atribuido ao elemento td agora */
-
-    var cell = document.createElement("td"); /* cria elemento td, célula da tabela */
-    cell.appendChild(seletor); // teste ##############
-    cell.appendChild(campo_tarefa); /* coloca o elemento output dentro do elemento td (celula) */
+    var celula = document.createElement("td"); /* cria elemento td, célula da tabela */
+    celula.setAttribute("id", "tarefa" + String(contador)); /* atribui o id em função do contador global */
+    celula.appendChild(seletor); /* coloca o elemento input (checkbox) dentro do elemento td (celula) */
     
-    var row = document.createElement("tr"); /* cria elemento tr, linha da tabela */
-    row.appendChild(cell); /* coloca o elemento td dentro de tr */
+    var linha = document.createElement("tr"); /* cria elemento tr, linha da tabela */
+    linha.appendChild(celula); /* coloca o elemento td dentro de tr */
 
     var ref_inserir = document.getElementsByTagName("tbody")[0]; /* pega a a primeira ocorrencia da tag tbody como referencia (?) */
-    ref_inserir.appendChild(row); /* coloca o elemento tr de tbody, no fim */
-
-    return campo_tarefa.getAttribute("id"); /* retorna o id do elemento de output criado para conter a tarefa */
+    ref_inserir.appendChild(linha); /* coloca o elemento tr dentro (no fim) de tbody */
+    
+    return celula; /* retorna o elemento td */
 }
 
 function recebe(){ /* recebe tarefa pelo campo de texto, chama funcao que cria uma linha na tabela com um campo de saida, e coloca a tarefa nesse campo */
-    var tarefa = document.getElementById("entrada_tarefa").value; /* recebe a tarefa pelo campo de input de identificado por id="entrada_tarefa" */
-    
-    document.getElementById(criar()).innerHTML = tarefa; /* insere a tarefa no elemento criado por criar() */
+    var tarefa_recebida = document.getElementById("entrada_tarefa").value; /* recebe a tarefa pelo campo de input" */
+    var no_tarefa = document.createTextNode(" " + tarefa_recebida); /* cria elemento nó de texto contendo a tarefa recebida */
+    criar().appendChild(no_tarefa); /* coloca o nó de texto no elemento td criado */
 }
 
 function mudar(){
@@ -49,5 +43,4 @@ function apagar(){
 REFERÊNCIAS:
 Como criar div com javascript - https://pt.stackoverflow.com/questions/187803/como-criar-div-com-javascript
 Adentrando uma tabela HTML com Java​Script e interfaces DOM - https://developer.mozilla.org/pt-BR/docs/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
-Forumlários: Checkboxes e radios - https://getbootstrap.com.br/docs/4.1/components/forms/#checkboxes-e-radios
 */
