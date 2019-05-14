3d69817369a355d69553db54f7490510e0d9d4a3
Bruno Cesar <bruunoocesar@gmail.com>
2019-05-14 16:05:09 -0300

    A função criar() está retornando o elemento <p> criado para conter o checkbox e a tarefa inserida. Porém, esses dois elementos estão sendo apresentados um em baixo do outro, diferente do resultado do teste feito no arquivo 'script_teste_DOM.html' em que o checkbox e o texto contido dentro de <p> são apresentados lado a lado.

--- myscripts.js
+++ myscripts.js
@@ -1,43 +1,39 @@
 var global = 0; /* variavel global para contar número de tarefas criadas*/
 
-function recebe_teste() { /* recebe tarefa pelo campo de texto e manda para o campo 'a fazer' */  
-    var tarefa = document.getElementById("entrada_tarefa").value;
-    document.getElementById("a_fazer").innerHTML = tarefa;
-}
-
 function testa_entrada(){ /* testa se a tarefa inserida é valida */
 
 }
 
-function criar(elemento_tbody){ /* cria uma linha/célula de tabela com um checkbox e retorna o elemento (td) célula, recebe o id do elemento tbody onde será adicionado */
+function criar(elemento_tbody){ /* recebe id do elemento <tbody> onde será criada uma linha/célula de tabela com dois elementos: checkbox e um parágrafo; retorna o elemento <p> criado */
     var contador = global++; /* incrementa o contador a cada nova tarefa inserida */
 
-    var seletor = document.createElement("input"); /* cria um elemento input */
+    var seletor = document.createElement("input"); /* cria um elemento <input> */
     seletor.setAttribute("type", "checkbox"); /* define o elemento input como checkbox */
     seletor.setAttribute("id", "faz_check" + String(contador)); /* atribui o id em função do contador global */
-    
-    var celula = document.createElement("td"); /* cria elemento td, célula da tabela */
-    celula.setAttribute("id", "faz_celula" + String(contador)); /* atribui o id em função do contador global */
-    celula.appendChild(seletor); /* coloca o elemento input (checkbox) dentro do elemento td (celula) */
-    
-    var linha = document.createElement("tr"); /* cria elemento tr, linha da tabela */
-    celula.setAttribute("id", "faz_linha" + String(contador)); /* atribui o id em função do contador global */
-    linha.appendChild(celula); /* coloca o elemento td dentro de tr */
 
-    var ref_inserir = document.getElementById(elemento_tbody); /* pega o elemento tbody pela id passada como argumento */
-    ref_inserir.appendChild(linha); /* coloca o elemento tr dentro (no fim) de tbody */
+    var paragrafo = document.createElement("p"); /* cria um elemento <p> */
+    paragrafo.setAttribute("id", "faz_par" + String(contador)); /* atribui o id em função do contador global */
     
-    return celula; /* retorna o elemento td */
+    var celula = document.createElement("td"); /* cria elemento <td>, célula da tabela */
+    celula.setAttribute("id", "faz_celula" + String(contador)); /* atribui o id em função do contador global */
+    celula.appendChild(seletor); /* coloca o elemento <input> (checkbox) dentro do elemento <td> (celula) */
+    celula.appendChild(paragrafo); /* coloca o elemento <p> dentro do elemento <td> */
+    
+    var linha = document.createElement("tr"); /* cria elemento <tr>, linha da tabela */
+    celula.setAttribute("id", "faz_linha" + String(contador)); /* atribui o id em função do contador global */
+    linha.appendChild(celula); /* coloca o elemento <td> dentro de <tr> */
+
+    var ref_inserir = document.getElementById(elemento_tbody); /* pega o elemento <tbody> pela id passada como argumento */
+    ref_inserir.appendChild(linha); /* coloca o elemento <tr> dentro (no fim) de <tbody> */
+    
+    return paragrafo; /* retorna o elemento <p> */
 }
 
-function recebe(){ /* recebe tarefa pelo campo de texto, chama funcao que cria uma linha na tabela com um campo de saida, e coloca a tarefa nesse campo */
+function recebe(){ /* recebe tarefa pelo campo de texto, chama funcao que cria uma linha na tabela com elemento paragráfo, e coloca a tarefa nesse elemento */
     var tarefa_recebida = document.getElementById("entrada_tarefa").value; /* recebe a tarefa pelo campo de input" */
     if(tarefa_recebida != ""){ /* garante que strings vazias não entrem como tarefas */
-
-        //var conteudo = document.createElement("output");
-
-        var no_tarefa = document.createTextNode(" " + tarefa_recebida); /* cria elemento nó de texto contendo a tarefa recebida */
-        criar("fazer").appendChild(no_tarefa); /* coloca o nó de texto no elemento td criado */
+        //var no_tarefa = document.createTextNode(" " + tarefa_recebida); /* cria elemento nó de texto contendo a tarefa recebida */
+        criar("fazer").appendChild(tarefa_recebida); /* coloca a tarefa recebida no elemento <p> criado pela função criar() */
     }
 }
 
@@ -46,9 +42,7 @@ function apagar(){
 }
 
 function mudar(){
-    var tarefa_para_fazer = 
-    var no_tarefa = document.createTextNode(" " + tarefa_para_fazer); /* cria elemento nó de texto contendo a tarefa recebida */
-    criar("fazendo_tabela").appendChild(no_tarefa); /* cria */
+    
 }
 
 function verifica_estado(){ /* verifica o estado de todos os checkbox que existem */
