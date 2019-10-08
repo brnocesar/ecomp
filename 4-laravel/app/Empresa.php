2fdcf74c202756adad5ea092e7632ee25477d362
Bruno Cesar <bruunoocesar@gmail.com>
2019-10-08 00:23:01 -0300

    Define as relações (entre Grupos e Empresas) nos Models

--- 4-laravel/app/Empresa.php
+++ 4-laravel/app/Empresa.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Grupo;
 class Empresa extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['nome', 'endereco'];
+
+    public function grupos(){
+        return $this->belongsToMany(Grupo::class);
+    }
 }
--- 4-laravel/app/Grupo.php
+++ 4-laravel/app/Grupo.php
@@ -4,10 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Empresa;
 
 class Grupo extends Model
 {
     use SoftDeletes;
 
     protected $fillable = ['nome'];
+
+    public function empresas(){
+        return $this->belongsToMany(Empresa::class);
+    }
 }
