34b73e22ee62c3f4577eb8dc7eab811dac18c3ed
Bruno Cesar <bruunoocesar@gmail.com>
2019-10-15 20:47:09 -0300

    Cria model e migration de usuário

---
+++ 4-laravel/database/migrations/2019_10_15_230422_create_usuarios_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->boolean('admin')->default(false);
+            $table->boolean('email_verificado')->default(false);
+            $table->string('senha');
+            $table->rememberToken();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
